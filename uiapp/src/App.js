d7dcba250aee84e250ca6c3769e8be2c8ae12854
lucas bianchi <lucmbianchi2000@gmail.com>
2023-04-27 16:43:01 -0300

    se agrega itemList

--- uiapp/src/App.js
+++ uiapp/src/App.js
@@ -1,11 +1,15 @@
 
+
 import './App.css';
 import AddProduct from './components/addProduct/AddProduct';
+import ItemList from './components/itemList/ItemList';
 
 function App() {
   return (
     <div className="App">
       <AddProduct/>
+      <ItemList/>
+     
     </div>
   );
 }
